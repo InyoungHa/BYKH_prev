e2089c1144e88cfdd4c7f7b7f013be39e8577328
unmarun <skmk426@gmail.com>
2023-05-16 08:46:49 +0900

    un

---
+++ src/main/java/com/bykh/groupware/adminController.java
@@ -0,0 +1,30 @@
+package com.bykh.groupware;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class adminController {
+	
+	//근태관리 출퇴근기록 페이지(메인)
+	@GetMapping("/attendanceManage")
+	public String attendanceManage() {
+		
+		return "content/attendanceManage";
+	}
+	
+	//근태관리 휴가관리 페이지
+	@GetMapping("/vacationManage")
+	public String vacationManage() {
+		
+		return "content/vacationManage";
+	}
+	
+	
+	//근태관리 연장근무관리 페이지
+		@GetMapping("/overTimeManage")
+		public String overTimeManage() {
+			
+			return "content/overTimeManage";
+		}
+}
