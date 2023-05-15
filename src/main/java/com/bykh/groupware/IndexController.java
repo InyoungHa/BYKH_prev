433e98c77bee81f9a061d1736eea4444bddacb15
unmarun <skmk426@gmail.com>
2023-05-15 17:23:06 +0900

    firstUn

--- src/main/java/com/bykh/groupware/IndexController.java
+++ src/main/java/com/bykh/groupware/IndexController.java
@@ -10,4 +10,18 @@ public class IndexController {
 	public String index() {
 		return "content/main";
 	}
+	
+	//근태관리 출퇴근기록 페이지
+	@GetMapping("/attendanceManage")
+	public String attendanceManage() {
+		
+		return "attendanceManage";
+	}
+	
+	//근태관리 휴가관리 페이지
+	@GetMapping("/vacationManage")
+	public String vacationManage() {
+		
+		return "vacationManage";
+	}
 }
